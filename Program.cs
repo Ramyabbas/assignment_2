65851d32b69535aa47c7f71022d254e25920fd9a
Ramy Abbas <Rami98abbas@gmail.com>
2020-10-09 22:11:30 +0200

    G-level
    This version of code is for G-level

--- Program.cs
+++ Program.cs
@@ -2,19 +2,17 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Net.Sockets;
-using System.Runtime.InteropServices;
+
 
 namespace assignment_2
 {
-    
+
 
     class Program
     {
         public static void writeProducts()
         {
-            var outFile = "/Users/diyar/Desktop/Ramy/Programmering med c#/Inlämningar/assignment_2/assignment_2/file.txt";
+            var outFile = "../../../products.txt";
             Product product = new Product("Banan", 4, 200);
             Product product1 = new Product("Cola", 15, 300);
             Product product2 = new Product("Cafe", 20, 400);
@@ -32,7 +30,7 @@ namespace assignment_2
 
         public static List<Product> readProducts()
         {
-            var inFile = "/Users/diyar/Desktop/Ramy/Programmering med c#/Inlämningar/assignment_2/assignment_2/file.txt";
+            var inFile = "../../../products.txt";
             var products = new List<Product>();
 
             using (StreamReader sr = File.OpenText(inFile))
@@ -51,7 +49,7 @@ namespace assignment_2
         {
             int position = -1;
 
-            for (int i=0; i< products.Count; i++)
+            for (int i = 0; i < products.Count; i++)
             {
                 if (productid == products[i].ID)
                 {
@@ -61,35 +59,155 @@ namespace assignment_2
             return position;
         }
 
-        public static void run(List<Product> products)
+        
+        public static string checkInputType(string input)
         {
-            var recepitRows = new List<RecepitRow>();
-            while (true)
+            string inputType = "";
+            var inputList = input.Split(' ');
+            if ((inputList.Count() == 1) && (input.ToUpper() == ("PAY")))
             {
-                try
-                {
+                inputType = "PAY";
+            }
+            else if ((inputList.Count() == 2) && (inputList[0].ToUpper() == "RETURN"))
+            {
+                inputType = "RETURN";
+            }
+            else if (inputList.Count() == 2)
+            {
+                inputType = "CONTINUE";
+            }
+            else
+            {
+                inputType = "ERROR";
+            }
+            return inputType;
 
-                    Console.WriteLine("kommandon:");
-                    Console.Write("<Produktid>");
-                    Console.Write(" < Antal >");
-                    Console.WriteLine("PAY");
-                    Console.Write("Komando:");
-                    int produktid = Convert.ToInt32(Console.ReadLine());
-                    int antal = Convert.ToInt32(Console.ReadLine());
-                    var product = products.Find(p => p.ID == produktid);
-                    var receiptRow = new RecepitRow(product.Name, antal, product.Price);
-                    recepitRows.Add(receiptRow);
-                    foreach(var recepitrow in recepitRows) 
-                    {
-                        recepitrow.printRow();
-                    }
-                }
-                catch(Exception ex)
-                {
-                    Console.WriteLine(ex);
-                }
+        }
+
+        public static int checkReturnInput(string input)
+        {
+            int productID;
+            var inputList = input.Split(' ');
+            bool result = int.TryParse(inputList[1], out productID);
+            if (result == false)
+            {
+                productID = -1;
+            }
+            return productID;
+        }
+
+        public static Tuple<int, int> checkContinueInput(string input)
+        {
+            var inputList = input.Split(' ');
+            int productID;
+            int quantity;
+
+            bool resultProductID = int.TryParse(inputList[0], out productID);
+            bool resultQuantity = int.TryParse(inputList[1], out quantity);
+
+            if ((resultProductID == true) && (resultQuantity == true))
+            {
+                return Tuple.Create(productID, quantity);
+            }
+            else
+            {
+                return Tuple.Create(-1, -1); // error in return command
             }
         }
+
+        
+        public static void run(List<Product> products)
+        {
+            var recepit = new Recepit();
+            while (true)
+            {
+                int productID = -1;
+                Product product;
+
+
+                Console.WriteLine("\n**Tillåtna kommandon**");
+                    
+                Console.WriteLine("PAY");
+                Console.WriteLine("RETURN <PRODUKTID>");
+                Console.WriteLine("<Produktid>  <Antal>");
+                   
+
+                string input = Console.ReadLine();
+
+                var inputType = checkInputType(input);
+
+                if (inputType == "ERROR")
+                {
+                    Console.WriteLine("Fel kommando");
+                }
+                else if (inputType == "PAY")
+                {
+                    if (recepit.RecepitRows.Count() != 0)
+                    {
+                        recepit.save();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Inget kvitto har sparats. Välkomen Åter :)");
+                    }
+                    break;
+                }
+                else if (inputType == "RETURN")
+                {
+                    if (recepit.RecepitRows.Count() != 0)
+                    {
+                        productID = checkReturnInput(input);
+
+                        if (productID == -1)
+                        {
+                            Console.WriteLine("Fel kommando");
+                        }
+                        else
+                        {
+                            productID = checkReturnInput(input);
+                            product = products.Find(p => p.ID == productID);
+                            if (product != null)
+                            {
+                                recepit.decreaseQuantity(productID);
+                                recepit.print();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Produkten finns ej!");
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    // CONTINUE
+                    var validatedInput = checkContinueInput(input);
+
+                    if (validatedInput.Item1 == -1)
+                    {
+                        Console.WriteLine("Fel kommando");
+                    }
+                    else
+                    {
+                        product = products.Find(p => p.ID == validatedInput.Item1);
+                        if (product != null)
+                        {
+                            var receiptRow = new RecepitRow(product.Name, validatedInput.Item2, product.Price, product.ID);
+                            recepit.addRecepitRow(receiptRow);
+                            recepit.print();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Produkten finns ej!");
+                        }
+                    }
+
+                }
+             
+            }
+        }
+
+
 
 
         static void Main(string[] args)
@@ -99,15 +217,21 @@ namespace assignment_2
             {
                 Console.WriteLine("KASSA");
                 Console.WriteLine("1. Ny kund");
+                Console.WriteLine("2. Adminverktyg");
                 Console.WriteLine("0. Avsluta");
                 var input = (Console.ReadLine());
                 if (input == "1")
                 {
                     run(products);
                 }
+                else if (input == "2")
+                {
+                    //edit(products);
+                }
+
                 else if(input == "0")
                 {
-                    
+                    break;
                 }
 
                 else
--- RecepitRow.cs
+++ RecepitRow.cs
@@ -5,22 +5,33 @@ namespace assignment_2
 {
     public class RecepitRow
     {
-        public string ProductName { get; private set; }
-        public int Quantity { get; private set; }
-        public int Price { get; private set; }
-        public int Sum { get; private set; }
+        public string ProductName {  get;  set; }
+        public int Quantity { get;  set; }
+        public int Price { get;  set; }
+        public int Sum { get;  set; }
+        public int Total { get;  set; }
+        public int ProductId { get; set; }
 
-        public RecepitRow(string productname, int quantity, int price)
+        public RecepitRow(string productname, int quantity, int price, int prodcutId)
         {
             ProductName = productname;
             Quantity = quantity;
             Price = price;
             Sum = price * quantity;
+            ProductId = prodcutId;
         }
         public void printRow()
         {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine($"{ProductName} {Quantity} * {Price}kr = {Sum}kr ");
+            Console.ResetColor();
 
         }
+        public void updateSum()
+        {
+            Sum = Price * Quantity;
+        }
+        
     }
 }
